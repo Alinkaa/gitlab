a0a65255c0721a6e3d39fe98db18c2fb32e63a78
Alinkaa <jankon@mail.ru>
2017-03-05 20:41:47 +0200

    add major code

--- s12.py
+++ s12.py
@@ -1,1 +1,10 @@
 import re
+hand = open(raw_input('Enter file name: '),'r') 
+numlist = [] 
+for line in hand: 
+    line = line.rstrip() 
+    stuff = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line) 
+    if len(stuff) != 1: continue
+    num = float(stuff[0]) 
+    numlist.append(num) 
+print "Maximum: " + str(max(numlist))
